5a866ce93648c9bc394c11496d8c027ff247ac71
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2023-10-16 18:25:00 +0530

    D flip flop simulation

---
+++ Simulation Sources/D_FF_A.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06/10/2023 03:20:14 AM
+-- Design Name: 
+-- Module Name: D_FF_A - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity D_FF_A is
+    Port ( D : in STD_LOGIC;
+           Res : in STD_LOGIC;
+           Clk : in STD_LOGIC;
+           Q : out STD_LOGIC;
+           Qbar : out STD_LOGIC);
+end D_FF_A;
+
+architecture Behavioral of D_FF_A is
+
+begin
+
+    process (Clk,Res) begin
+        if (Res = '1') then
+            Q <= '0';
+            Qbar <= '1';
+        elsif (rising_edge(Clk)) then
+            Q <= D;
+            Qbar <= not D;
+        end if;
+    end process;
+
+end Behavioral;

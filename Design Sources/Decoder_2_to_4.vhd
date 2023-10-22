d52c3b8a25ab036a446c5e0dc2b860101125fece
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2023-10-22 10:24:52 +0530

    Update

--- Design Sources/Decoder_2_to_4.vhd
+++ Design Sources/Decoder_2_to_4.vhd
@@ -32,18 +32,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Decoder_2_to_4 is
-    Port ( I : in STD_LOGIC_VECTOR (1 downto 0);
-           EN : in STD_LOGIC;
-           Y : out STD_LOGIC_VECTOR (3 downto 0));
+    Port (
+        I  : in  STD_LOGIC_VECTOR (1 downto 0);
+        EN : in  STD_LOGIC;
+        Y  : out STD_LOGIC_VECTOR (3 downto 0)
+    );
 end Decoder_2_to_4;
 
 architecture Behavioral of Decoder_2_to_4 is
-
 begin
-
-    Y(0) <= (NOT(I(0))) AND (NOT(I(1))) AND EN;
-    Y(1) <= I(0) AND (NOT(I(1))) AND EN;
-    Y(2) <= (NOT(I(0))) AND I(1) AND EN;
-    Y(3) <= I(0) AND I(1) AND EN;
-
+    process (I, EN)
+    begin
+        if EN = '1' then
+            case I is
+                when "00" => Y <= "0001";
+                when "01" => Y <= "0010";
+                when "10" => Y <= "0100";
+                when "11" => Y <= "1000";
+                when others => Y <= (others => '0');
+            end case;
+        else
+            Y <= (others => '0');
+        end if;
+    end process;
 end Behavioral;
